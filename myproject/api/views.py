7735dd03b6f1c4a66b309204bdbb872ebce1b9c5
Nevil Ndubi <ndubinevil@gmail.com>
2024-09-10 00:05:46 +0300

    views.py

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# My views are here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
+@api_view(['GET'])
+def get_user(request):
+    return Response(UserSerializer({'name': 'John', 'email': 'john@example.com'}))
