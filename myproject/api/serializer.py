d499c319fcc4cee563fb6083f79a1358d5f8eff2
Nevil Ndubi <ndubinevil@gmail.com>
2024-09-10 11:00:36 +0300

    investment_bank

--- myproject/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'name', 'email', 'age'] # This explicitly defines the fields I have to serialize.
